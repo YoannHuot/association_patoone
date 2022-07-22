0da1162efe17e5aaa8eb904c67512c553d99b75f
Yoann <contact.yoann.huot@gmail.com>
2022-07-22 14:38:58 +0200

    add logo, refactoring code, add meta tags, change css page dogs, correction of spelling mistakes, compress img, w3c check for html

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const dogsObject = {
 		loveCats: true,
 		appartement: false,
 		description:
-			"Chiko est un adorable petit chien. Il a été retrouvé attaché à un poteau et amaigri. Aujourd'hui Chiko va mieux, il est en bonne santé et s'amuse beaucoup. Il apprécie la présence de ses congénères et, jusqu'à ce jour, n'a pas montré d'inquiétudes particulières vis à vis d'autres animaux ",
+			"Chiko est un adorable petit chien. Il a été retrouvé attaché à un poteau et amaigri. Aujourd'hui Chiko va mieux, il est en bonne santé et s'amuse beaucoup. Il apprécie la présence de ses congénères et, jusqu'à ce jour, n'a pas montré d'inquiétudes particulières vis à vis d'autres animaux.",
 		profil: "Chiko s'avère être un chien facile sans séquelle psychologique ou physique majeur.",
 		dogImg: "/images/neutralBoy.jpg",
 	},
@@ -19,8 +19,8 @@ const dogsObject = {
 		loveCats: false,
 		appartement: true,
 		description:
-			"Stella est une adorable chienne au passé douloureux. Elle a été délaissée à contrecoeur à l'association suite au décès de son maître et ami. En bonne santé, elle est toutefois atteinte de la leishmaniose et nécessite donc une attention particulière et un traitement à vie",
-		profil: "Si Stella adore les humains, elle n'en est pas moins une caractérielle qui se laisse difficillement approchée par ses congénères ou d'autres animaux. De part sa petite taille, Stelle s'adaptera à une vie en appartement",
+			"Stella est une adorable chienne au passé douloureux. Elle a été délaissée à contrecoeur à l'association suite au décès de son maître et ami. En bonne santé, elle est toutefois atteinte de la leishmaniose et nécessite donc une attention particulière et un traitement à vie.",
+		profil: "Si Stella adore les humains, elle n'en est pas moins une caractérielle qui se laisse difficilement approchée par ses congénères ou d'autres animaux. Du fait de sa petite taille, Stelle s'adaptera à une vie en appartement.",
 		dogImg: "/images/animal.png",
 	},
 	andre: {
@@ -31,7 +31,7 @@ const dogsObject = {
 		loveCats: false,
 		appartement: true,
 		description:
-			"Andre est un Shiba d'âge moyenne. Il a été secouru de son précédent foyer où il était maltraité par ses anciens propriétaires. Il apprécie sa vie en famille d'acceuil mais garde toutefois des réservers vis-à-vis des humains. Il faudra du temps, de la patience et la compassion pour qu'il se livre à nouveau.  ",
+			"Andre est un Shiba d'âge moyenne. Il a été secouru de son précédent foyer où il était maltraité par ses anciens propriétaires. Il apprécie sa vie en famille d'accueil mais garde toutefois des réserves vis-à-vis des humains. Il faudra du temps, de la patience et la compassion pour qu'il se livre à nouveau.  ",
 		profil: "Andre est un chien au passé lourd qui préférera une famille d'expérience. Adepte de siestes et de canapé, une vie en appartement lui conviendra tout à fait.",
 		dogImg: "/images/animal.png",
 	},
--- dogs.js
+++ dogs.js
@@ -1,8 +1,8 @@
-const maison = "Une maison est nécessaire et sera un pré-requis obligatoire ";
+const maison = "Une maison est nécessaire et sera un prérequis obligatoire ";
 const appartement = "Une vie en appartement est envisageable";
 const noCats = "Un foyer sans chat est nécessaire";
 const cats = "Un foyer avec des chats ne pose pas de problème";
-const noOtherDogs = "Un foyer sans autre chien sera un pré-requis obligatoire";
+const noOtherDogs = "Un foyer sans autre chien sera un prérequis obligatoire";
 const otherDogs = "Un foyer avec d'autres chiens serait un plus";
 
 const showDog = () => {
@@ -13,31 +13,35 @@ const showDog = () => {
 };
 
 const createDom = (p, data) => {
-	const capitalizeFirstLetter = (string) => {
-		return string.charAt(0).toUpperCase() + string.slice(1);
-	};
-	const name = capitalizeFirstLetter(data.name);
-
-	if (p && data) {
+	if (p === null || data === null || p === undefined) {
+		$(".chiens-description").append("<div>Comment êtes-vous arrivés ici ? Vous n'avez pas choisi de chien.</div>");
+		setTimeout(() => {
+			alert("Vous n'avez pas choisi de chien à regarder. Vous allez être redirigé à l'accueil");
+			window.location.replace("/");
+		});
+	} else if (p && data) {
 		const path = `images/${data.name}/${data.name}`;
-		console.log(path);
+		const capitalizeFirstLetter = (string) => {
+			return string.charAt(0).toUpperCase() + string.slice(1);
+		};
+		const name = capitalizeFirstLetter(data.name);
 
 		$(".content__section").append(` 
-        <h2 class="text-capitalize px-4 pt-4">${name}</h5>
+        <h2 class="text-capitalize px-4 pt-4">${name}</h2>
         <div >
-            <div class="row px-4">
+            <div class="row ">
 			<div class="dogs__description col-lg-4 col-md-12 mb-4 mb-lg-0 ">
 				<img
                 src=${`${path}1.jpg`}
 					class="w-100 img-fluid pictures__chiens  rounded mb-4"
-					alt="Boat on Calm Water"
+					alt=${`${data.name} `}
 				
 				/>
 
 				<img
                 src=${`${path}2.jpg`}
 					class="w-100 img-fluid pictures__chiens  rounded mb-4"
-					alt="Wintry Mountain Landscape"
+					alt=${`${data.name} `}
 				
 				/>
 			</div>
@@ -45,35 +49,27 @@ const createDom = (p, data) => {
 			<div class="column__chiens col-lg-4 mb-4 mb-lg-0 ">
 				<img
                 src=${`${path}3.jpg`}
-					class="w-100 img-fluid pictures__chiens  rounded mb-4"
-					alt="Mountains in the Clouds"
-				
-				/>
+					class="w-100 img-fluid pictures__chiens rounded mb-4"
+					alt=${`${data.name}`} />
 
 				<img
                 src=${`${path}4.jpg`}
 					class="w-100 img-fluid pictures__chiens rounded mb-4"
-					alt="Boat on Calm Water"
-				
-				/>
+					alt=${`${data.name}`} />
 			</div>
 
 			<div class="col-lg-4 mb-4 mb-lg-0 ">
 				<img
                 src=${`${path}5.jpg`}
-					class="w-100 img-fluid  rounded mb-4 pictures__chiens"
-				 alt="Waves at Sea"
-				
-				/>
-
+					class="w-100 img-fluid pictures__chiens rounded mb-4 "
+				  alt=${`${data.name}`} />
 				<img
                 src=${`${path}6.jpg`}
-					class="w-100 img-fluid pictures__chiens  rounded mb-4"
-					alt="Yosemite National Park"
-				
-				/>
+					class="w-100 img-fluid pictures__chiens rounded mb-4"
+					alt=${`${data.name}`} />
 			</div>
 		</div>
+
         <div class="px-4">
 			<h3 class="pt-4">Un peu plus sur ${name}</h3>
 			<p >${data.description}</p>
@@ -84,31 +80,23 @@ const createDom = (p, data) => {
         <div class="section__background row w-100 mt-4">
             <div class="infos col-sm px-4 mt-4 pt-4 d-flex align-items-center flex-column" style="height:250px">
                 <p class="text-center" style="height:100px">${data.appartement ? appartement : maison}</p>
-				<img src = "${
+				<img  src = "${
 					data.appartement ? "images/pictos/apartment.png" : "images/pictos/house.png"
-				}" alt="appartement permission" class="picto__chiens w-25 "/>
+				}" alt="appartement permission" class="picto__chiens w-25"/>
             </div>
-        <div class="infos col-sm px-4 mt-4 pt-4 d-flex align-items-center flex-column" style="height:250px">
-            <p class="text-center" style="height:100px">${data.loveCats ? cats : noCats}</p>
-			<img src = "${
-				data.loveCats ? "images/pictos/cat.png" : "images/pictos/no-cats.png"
-			}" alt="cats permission" class="picto__chiens w-25"/>
-        </div>
 			<div class="infos col-sm px-4 mt-4 pt-4 d-flex align-items-center flex-column" style="height:250px">
-			<p class="text-center" style="height:100px">${data.loveFriends ? otherDogs : noOtherDogs}</p>
-			<img src = "${
-				data.loveFriends ? "images/pictos/dogs-allowed.png" : "images/pictos/no-dogs.png"
-			}" alt="cats permission" class="picto__chiens w-25"/>
-        </div>
-</div>`);
-	} else if (p === null || data === null) {
-		$(".chiens-description").append(
-			"<div>Comment êtes vous arrivés ici ? Vous n'avez pas choisi de chien :/ </div>"
-		);
-		setTimeout(() => {
-			alert("Vous n'avez pas choisi de chiens à regarder. Vous allez être redirigié à l'acceuil");
-			window.location.replace("/");
-		});
+				<p class="text-center" style="height:100px">${data.loveCats ? cats : noCats}</p>
+				<img authorized" src = "${
+					data.loveCats ? "images/pictos/cat.png" : "images/pictos/no-cats.png"
+				}" alt="cats permission" class="picto__chiens w-25"/>
+			</div>
+			<div class="infos col-sm px-4 mt-4 pt-4 d-flex align-items-center flex-column" style="height:250px">
+				<p class="text-center" style="height:100px">${data.loveFriends ? otherDogs : noOtherDogs}</p>
+				<img  src = "${
+					data.loveFriends ? "images/pictos/dogs-allowed.png" : "images/pictos/no-dogs.png"
+				}" alt="dogs permission" class="picto__chiens w-25"/>
+       		 </div>
+		</div>`);
 	}
 };
 
